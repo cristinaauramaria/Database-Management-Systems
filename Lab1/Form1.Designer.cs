8b06fdc0cbad05ba3cf37178be7958c423e0a40f
cristinaauramaria <126888996+cristinaauramaria@users.noreply.github.com>
2023-05-19 00:31:45 +0300

    Add files via upload

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace WF_App_222_2023
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            dataGridViewParent = new DataGridView();
+            labelParent = new Label();
+            dataGridViewChild = new DataGridView();
+            labelChild = new Label();
+            Delete = new Button();
+            textNume = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textCnp = new TextBox();
+            Add = new Button();
+            Update = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewParent).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewChild).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridViewParent
+            // 
+            dataGridViewParent.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridViewParent.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewParent.Location = new Point(101, 113);
+            dataGridViewParent.Margin = new Padding(4);
+            dataGridViewParent.Name = "dataGridViewParent";
+            dataGridViewParent.RowHeadersWidth = 51;
+            dataGridViewParent.RowTemplate.Height = 29;
+            dataGridViewParent.Size = new Size(425, 421);
+            dataGridViewParent.TabIndex = 0;
+            dataGridViewParent.CellClick += dataGridViewParent_CellClick;
+            // 
+            // labelParent
+            // 
+            labelParent.AutoSize = true;
+            labelParent.BackColor = Color.DarkSlateBlue;
+            labelParent.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            labelParent.ForeColor = SystemColors.ButtonHighlight;
+            labelParent.Location = new Point(221, 71);
+            labelParent.Margin = new Padding(4, 0, 4, 0);
+            labelParent.Name = "labelParent";
+            labelParent.Size = new Size(208, 38);
+            labelParent.TabIndex = 1;
+            labelParent.Text = "Tabelul Functii";
+            // 
+            // dataGridViewChild
+            // 
+            dataGridViewChild.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridViewChild.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewChild.Location = new Point(633, 113);
+            dataGridViewChild.Margin = new Padding(4);
+            dataGridViewChild.Name = "dataGridViewChild";
+            dataGridViewChild.RowHeadersWidth = 51;
+            dataGridViewChild.RowTemplate.Height = 29;
+            dataGridViewChild.Size = new Size(527, 247);
+            dataGridViewChild.TabIndex = 2;
+            // 
+            // labelChild
+            // 
+            labelChild.AutoSize = true;
+            labelChild.BackColor = Color.DarkSlateBlue;
+            labelChild.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            labelChild.ForeColor = SystemColors.ButtonHighlight;
+            labelChild.Location = new Point(834, 71);
+            labelChild.Margin = new Padding(4, 0, 4, 0);
+            labelChild.Name = "labelChild";
+            labelChild.Size = new Size(230, 38);
+            labelChild.TabIndex = 3;
+            labelChild.Text = "Tabelul Angajati";
+            // 
+            // Delete
+            // 
+            Delete.BackColor = Color.DarkSlateBlue;
+            Delete.FlatStyle = FlatStyle.Flat;
+            Delete.ForeColor = SystemColors.ButtonHighlight;
+            Delete.Location = new Point(894, 500);
+            Delete.Name = "Delete";
+            Delete.Size = new Size(112, 34);
+            Delete.TabIndex = 5;
+            Delete.Text = "Delete";
+            Delete.UseVisualStyleBackColor = false;
+            Delete.Click += Delete_Click;
+            // 
+            // textNume
+            // 
+            textNume.Location = new Point(834, 377);
+            textNume.Name = "textNume";
+            textNume.Size = new Size(262, 31);
+            textNume.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.DarkSlateBlue;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(734, 383);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 25);
+            label1.TabIndex = 7;
+            label1.Text = "Nume: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.DarkSlateBlue;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(734, 432);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 25);
+            label2.TabIndex = 9;
+            label2.Text = "Cnp: ";
+            // 
+            // textCnp
+            // 
+            textCnp.Location = new Point(834, 426);
+            textCnp.Name = "textCnp";
+            textCnp.Size = new Size(262, 31);
+            textCnp.TabIndex = 8;
+            // 
+            // Add
+            // 
+            Add.BackColor = Color.DarkSlateBlue;
+            Add.FlatStyle = FlatStyle.Flat;
+            Add.ForeColor = SystemColors.ButtonHighlight;
+            Add.Location = new Point(734, 500);
+            Add.Name = "Add";
+            Add.Size = new Size(112, 34);
+            Add.TabIndex = 10;
+            Add.Text = "Add";
+            Add.UseVisualStyleBackColor = false;
+            Add.Click += Add_Click;
+            // 
+            // Update
+            // 
+            Update.BackColor = Color.DarkSlateBlue;
+            Update.FlatStyle = FlatStyle.Flat;
+            Update.ForeColor = SystemColors.ButtonHighlight;
+            Update.Location = new Point(1048, 500);
+            Update.Name = "Update";
+            Update.Size = new Size(112, 34);
+            Update.TabIndex = 11;
+            Update.Text = "Update";
+            Update.UseVisualStyleBackColor = false;
+            Update.Click += Update_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Teal;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1265, 622);
+            Controls.Add(Update);
+            Controls.Add(Add);
+            Controls.Add(label2);
+            Controls.Add(textCnp);
+            Controls.Add(label1);
+            Controls.Add(textNume);
+            Controls.Add(Delete);
+            Controls.Add(labelChild);
+            Controls.Add(dataGridViewChild);
+            Controls.Add(labelParent);
+            Controls.Add(dataGridViewParent);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Aplicatie Windows Forms 222";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridViewParent).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewChild).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridViewParent;
+        private Label labelParent;
+        private DataGridView dataGridViewChild;
+        private Label labelChild;
+        private Button Delete;
+        private TextBox textNume;
+        private Label label1;
+        private Label label2;
+        private TextBox textCnp;
+        private Button Add;
+        private Button Update;
+    }
+}
---
+++ Lab1/Form1.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace WF_App_222_2023
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Server=DESKTOP-4BATE1U\SQLEXPRESS;Database=Dealership_Auto_Final;Integrated Security=true;Trust Server Certificate=true;";
+        SqlDataAdapter da = new SqlDataAdapter();
+        BindingSource bsP = new BindingSource();
+        BindingSource bsC = new BindingSource();
+        DataSet dsP = new DataSet();
+        DataSet dsC = new DataSet();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    MessageBox.Show(connection.State.ToString());
+
+                    da.SelectCommand = new SqlCommand("SELECT * FROM Funcție", connection);
+                    dsP.Clear();
+                    da.Fill(dsP);
+                    dataGridViewParent.DataSource = dsP.Tables[0];
+                    bsP.DataSource = dsP.Tables[0];
+
+                    bsP.MoveLast();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridViewParent_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridViewParent.Rows[e.RowIndex].Cells[e.ColumnIndex].Value == null)
+                return;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string cod_f = dataGridViewParent.CurrentRow.Cells[0].Value.ToString();
+                    da.SelectCommand = new SqlCommand("SELECT * from Angajat " +
+                    "where cod_f = " + cod_f + "; ", connection);
+                    dsC.Clear();
+                    da.Fill(dsC);
+                    dataGridViewChild.DataSource = dsC.Tables[0];
+                    bsC.DataSource = dsC.Tables[0];
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewParent.SelectedCells.Count == 0)
+            {
+                MessageBox.Show("O linie in parinte trebuie selectata!");
+                return;
+            }
+            else if (dataGridViewParent.SelectedCells.Count > 1)
+            {
+                MessageBox.Show("O singura linie in parinte trebuie selectata!");
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    da.InsertCommand = new SqlCommand("INSERT INTO Angajat(nume_a,cnp_a,cod_f)" + " VALUES (@nume, @cnp, @cod_f);", connection);
+                    da.InsertCommand.Parameters.Add("@nume", SqlDbType.VarChar).Value = textNume.Text;
+                    da.InsertCommand.Parameters.Add("@cnp", SqlDbType.VarChar).Value = textCnp.Text;
+                    da.InsertCommand.Parameters.Add("@cod_f", SqlDbType.Int).Value = dsP.Tables[0].Rows[dataGridViewParent.CurrentRow.Index][0];
+                    da.InsertCommand.ExecuteNonQuery();
+                    MessageBox.Show("Adaugat cu succes!");
+
+                    textNume.Clear();
+                    textCnp.Clear();
+
+                    // actualizare tabel fiu cu noile date
+                    dsC.Clear();
+                    da.SelectCommand = new SqlCommand("SELECT * from Angajat WHERE cod_f = @cod_f", connection);
+                    da.SelectCommand.Parameters.Add("@cod_f", SqlDbType.Int).Value = dsP.Tables[0].Rows[dataGridViewParent.CurrentRow.Index][0];
+                    da.Fill(dsC);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewChild.SelectedCells.Count == 0)
+            {
+                MessageBox.Show("O linie in copil trebuie selectata!");
+                return;
+            }
+            else if (dataGridViewChild.SelectedCells.Count > 1)
+            {
+                MessageBox.Show("O singura linie in copil trebuie selectata!");
+                return;
+            }
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    da.DeleteCommand = new SqlCommand("Delete from Angajat where cod_a = @id;", connection);
+                    da.DeleteCommand.Parameters.AddWithValue("@id", (int)dataGridViewChild.CurrentRow.Cells[0].Value);
+                    da.DeleteCommand.ExecuteNonQuery();
+                    MessageBox.Show("Delete succes!");
+
+                    // actualizare tabel fiu cu noile date
+                    dsC.Clear();
+                    da.SelectCommand = new SqlCommand("SELECT * from Angajat WHERE cod_f = @cod_f", connection);
+                    da.SelectCommand.Parameters.Add("@cod_f", SqlDbType.Int).Value = dsP.Tables[0].Rows[dataGridViewParent.CurrentRow.Index][0];
+                    da.Fill(dsC);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewChild.SelectedCells.Count == 0)
+            {
+                MessageBox.Show("O linie in copil trebuie selectata!");
+                return;
+            }
+            else if (dataGridViewChild.SelectedCells.Count > 1)
+            {
+                MessageBox.Show("O singura linie in copil trebuie selectata!");
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    int x;
+                    connection.Open();
+                    da.UpdateCommand = new SqlCommand("Update Angajat set nume_a=@nume, cnp_a=@cnp, cod_f=@cod_f where cod_a=@id", connection);
+                    da.UpdateCommand.Parameters.AddWithValue("@nume", (string)dataGridViewChild.CurrentRow.Cells[1].Value);
+                    da.UpdateCommand.Parameters.AddWithValue("@cnp", (string)dataGridViewChild.CurrentRow.Cells[2].Value);
+                    da.UpdateCommand.Parameters.AddWithValue("@cod_f", (int)dataGridViewChild.CurrentRow.Cells[3].Value);
+                    da.UpdateCommand.Parameters.AddWithValue("@id", (int)dataGridViewChild.CurrentRow.Cells[0].Value);
+
+
+                    x = da.UpdateCommand.ExecuteNonQuery();
+                    if (x >= 1)
+                    {
+                        MessageBox.Show("The record has been updated");
+                    }
+
+                    // actualizare tabel fiu cu noile date
+                    dsC.Clear();
+                    da.SelectCommand = new SqlCommand("SELECT * from Angajat WHERE cod_f = @cod_f", connection);
+                    da.SelectCommand.Parameters.Add("@cod_f", SqlDbType.Int).Value = dsP.Tables[0].Rows[dataGridViewParent.CurrentRow.Index][0];
+                    da.Fill(dsC);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
